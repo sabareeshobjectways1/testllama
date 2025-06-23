3ce357808d65570691749ea9497e41825e293c68
sabareeshobjectways1 <sabareeshobjectways@gmail.com>
2025-06-23 22:25:59 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Web LLM API</h1>
+      <p>Use <code>/api/llama?prompt=Your+text+here</code> to interact with the model.</p>
+    </div>
+  );
+}
